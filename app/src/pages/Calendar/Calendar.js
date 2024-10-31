0c200b71b35d530a11113cbc9e6d874e29a8218e
AndreStenn <129811357+AndreStenn@users.noreply.github.com>
2024-10-31 13:38:38 -0400

    Implemented basic function of calendar

--- app/src/pages/Calendar/Calendar.js
+++ app/src/pages/Calendar/Calendar.js
@@ -1,7 +1,19 @@
 import React from 'react';
+import Calendar from 'react-calendar';
+import { useState } from 'react';
 
-export default function Calendar() {
-  return(
-    <h2>Calendar</h2>
+
+export default function CalendarUF() {
+  const [value, setValue] = useState(new Date());
+
+  function onChange(nextValue) {
+    setValue(nextValue);
+  }
+
+  return (
+    <Calendar
+      onChange={onChange}
+      value={value}
+    />
   );
 }
