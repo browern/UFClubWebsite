559c3ff55c7b8af13b13614d730a48e4bf9f775a
AndreStenn <129811357+AndreStenn@users.noreply.github.com>
2024-10-31 23:41:32 -0400

    Fixed month and added a sample event to calendar

--- app/src/pages/Calendar/Calendar.js
+++ app/src/pages/Calendar/Calendar.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import Calendar from 'react-calendar';
-import { useState } from 'react';
+//Imports calendar
+import { Calendar, CalendarProps, momentLocalizer } from 'react-big-calendar';
+//Localizer for calendar to keep track of time
+import moment from 'moment';
+//Styling for calendar
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import './index.css';
 
+// Referenced from: https://medium.com/@dhruvil_41022/how-to-make-reactjs-calendar-with-events-13bcea757525
+// Along with big-calendar documentation found here: https://jquense.github.io/react-big-calendar/examples/index.html?path=/story/about-big-calendar--page
 
-export default function CalendarUF() {
-  const [value, setValue] = useState(new Date());
+// Setups the localizer by providing the moment object
+const localizer = momentLocalizer(moment)
 
-  function onChange(nextValue) {
-    setValue(nextValue);
+const events = [
+  {
+    start: moment("2024-11-01 01:00:00").toDate(),
+    end: moment("2024-11-01 04:00:00").toDate(),
+    title: "tester testing",
   }
 
-  return (
-    <Calendar
-      onChange={onChange}
-      value={value}
-    />
-  );
-}
+];
+
+export default function EventCalendar() {
+  return <Calendar
+  events={events}
+  localizer={localizer}
+  views={['month', 'week', 'day']}
+  style = {{height: 700, width: '60%'}} />
+}; 
